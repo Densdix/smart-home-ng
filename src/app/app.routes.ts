ba88f7f36f3f41a3ab1061b98c3883273c5abfbf
Densdix <dixfun333333@gmail.com>
2025-08-08 00:14:36 +0300

    feat: update routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard';
+import { LoginComponent } from './auth/login';
+import { NotFoundComponent } from './not-found/not-found';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -8,7 +11,25 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: 'dashboard',
+    redirectTo: '/dashboard/overview/overview',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard/:dashboardId/:tabId',
     component: DashboardComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/404',
   },
 ];
